77424ea62f64ff0b7f423c1173f30a279c5e3446
Brandon Huffman <bt_huffman@msn.com>
2019-03-04 15:10:14 -0700

    Fixing javascript function

--- pro-dev-refurbished/js/script.js
+++ pro-dev-refurbished/js/script.js
@@ -9,6 +9,10 @@ let visualPercent = Math.round(visual/total*100);
 let auditoryPercent = Math.round(auditory/total*100);
 console.log("Auditory: " + auditoryPercent + "<br/>" + "Visual: " + visualPercent + "<br/>" + "Tactile: " + tactilePercent + "<br/>");
 
-document.getElementById("tactile").innerText = tactilePercent.toString();
+// document.getElementById("tactile").innerText = tactilePercent.toString();
 document.getElementById("visual").innerText = visualPercent.toString();
 document.getElementById("auditory").innerText = auditoryPercent.toString();
+
+let tactile = tactilePercent.toString();
+document.getElementById("tactile").innerHTML;
+
